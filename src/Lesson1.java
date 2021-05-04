4a8ab45d7c2e61c98ece66113005eae2fedcdd56
Pira4791 <Artur2111094@mail.ru>
2021-05-04 19:08:09 +0300

    1st load

---
+++ src/Lesson1.java
@@ -0,0 +1,23 @@
+public class Lesson1 {
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 20 ; i++)
+        {
+
+            if (i < 10) {
+                System.out.println("Я меньше 10 " );
+            } else {
+                System.out.println("Я больше 10 ");
+
+            }
+            if (i == 10) {
+                System.out.println("Я равен 10");
+            }
+
+
+        }
+
+        }
+    }
+
